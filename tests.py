6b8febbef2c25e08ab7411cef900912bce794706
chris <christianspiro1@gmail.com>
2025-10-24 18:31:57 -0400

    working on tests for files too

---
+++ tests.py
@@ -0,0 +1,6 @@
+from functions import get_files_info
+
+get_files_info("calculator", ".")
+get_files_info("calculator", "pkg")
+get_files_info("calculator", "/bin")
+get_files_info("calculator", "../")
