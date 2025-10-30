a5adfdf71abfde0048c6599147657628f0648f96
sudoplswork <christianspiro1@gmail.com>
2025-10-30 12:59:36 -0400

    refactor pt 1

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,9 +1,18 @@
 import os
 from google.genai import types
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-from functions.run_python_file import run_python_file
-from functions.write_file import write_file
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+        ]
+    )
 
 
 
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from config import SYSTEM_PROMPT
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
-from functions.call_function import call_function
+
+from functions.call_function import call_function, available_functions
 
 
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+
+
+
 
 
 
@@ -35,20 +35,15 @@ def main():
     )
     
     args = parser.parse_args()
-
     user_prompt = args.prompt
+
+
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_file,
-        schema_write_file
-        ]
-    )
+ 
 
     print("Your right hand man says: ")
     gemini_response = client.models.generate_content(
